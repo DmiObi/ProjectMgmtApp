a2188f752d379b4b941993eeb1195de58bea8a6f
Dmitirii <Dmitrii.Obideiko@gmail.com>
2022-07-12 18:01:38 -0500

    Added more features

---
+++ client/src/queries/ProjectQuries.js
@@ -0,0 +1,33 @@
+import { gql } from '@apollo/client';
+
+const GET_PROJECTS = gql`
+    query GET_PROJECTS {
+        projects {
+            id,
+            name,
+            status
+        }
+    }
+
+`
+
+const GET_PROJECT = gql`
+    query getProject($id: ID!) {
+        project(id: $id) {
+            id,
+            name,
+            description,
+            status,
+            client {
+                id,
+                name,
+                email,
+                phone
+            }
+        }
+    }
+
+`
+
+
+export { GET_PROJECT, GET_PROJECTS }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const colors = require('colors');
+const cors = require('cors');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
@@ -8,12 +9,13 @@ const port = process.env.PORT || 1000;
 
 const app = express();
 
+app.use(cors());
+
 // Connect to database
 connectDB();
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === 'development',
-
 }))
 
 app.listen(port, console.log(`Server running on port ${port}`))
